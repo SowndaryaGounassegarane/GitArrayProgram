57ce730643f45ec4fdd45710a3304fc28633fd14
SowndaryaGounassegarane <sowndarya.g@velankanigroup.com>
2025-04-14 15:36:39 +0530

    inital commit

---
+++ src/org/test/GitCode.java
@@ -0,0 +1,22 @@
+package org.test;
+
+public class GitCode {
+public void a1()
+{
+	System.out.println(1);
+}
+public void a2()
+{
+	System.out.println(1);
+}
+public void a3()
+{
+	System.out.println(3);
+}
+public void a4()
+{
+	System.out.println(4);
+}
+
+
+}
---
+++ src/org/test/GitCode1.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class GitCode1 {
+public void a4()
+{
+	System.out.println(4);
+	System.out.println(5);
+	
+	}
+}
+
